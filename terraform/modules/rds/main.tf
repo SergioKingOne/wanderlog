c115b3896da484e2d1929eb4129bdf825e69776c
Sergio <sergiorobayorr@gmail.com>
2024-12-14 19:12:23 -0500

    correct rds config

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -34,13 +34,13 @@ resource "aws_security_group" "rds_sg" {
 resource "aws_db_instance" "default" {
   identifier                 = "wanderlog-db"
   engine                     = "postgres"
-  engine_version             = "14.5"
+  engine_version             = "16.3"
   instance_class             = "db.t3.micro"
   allocated_storage          = 20
   storage_type               = "gp2"
-  username                   = var.db_username
+  username                   = "dbmaster"
   password                   = var.db_password
-  parameter_group_name       = "default.postgres14"
+  parameter_group_name       = "default.postgres16"
   skip_final_snapshot        = true
   publicly_accessible        = false
   vpc_security_group_ids     = [aws_security_group.rds_sg.id]
